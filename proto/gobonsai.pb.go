536fb8341b37e6aa64213cf13c3ea221af2a88f6
dragosbudan1 <dragos.budan@gmail.com>
2020-08-15 21:27:13 +0100

    added get pumps

--- proto/gobonsai.pb.go
+++ proto/gobonsai.pb.go
@@ -147,6 +147,146 @@ func (x *SetPumpReply) GetSuccess() bool {
 	return false
 }
 
+type PumpData struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	PumpId int32 `protobuf:"varint,1,opt,name=pumpId,proto3" json:"pumpId,omitempty"`
+	Active bool  `protobuf:"varint,2,opt,name=active,proto3" json:"active,omitempty"`
+}
+
+func (x *PumpData) Reset() {
+	*x = PumpData{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_gobonsai_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PumpData) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PumpData) ProtoMessage() {}
+
+func (x *PumpData) ProtoReflect() protoreflect.Message {
+	mi := &file_gobonsai_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PumpData.ProtoReflect.Descriptor instead.
+func (*PumpData) Descriptor() ([]byte, []int) {
+	return file_gobonsai_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *PumpData) GetPumpId() int32 {
+	if x != nil {
+		return x.PumpId
+	}
+	return 0
+}
+
+func (x *PumpData) GetActive() bool {
+	if x != nil {
+		return x.Active
+	}
+	return false
+}
+
+type GetPumpsResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Pumps []*PumpData `protobuf:"bytes,1,rep,name=Pumps,json=pumps,proto3" json:"Pumps,omitempty"`
+}
+
+func (x *GetPumpsResponse) Reset() {
+	*x = GetPumpsResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_gobonsai_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GetPumpsResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetPumpsResponse) ProtoMessage() {}
+
+func (x *GetPumpsResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_gobonsai_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetPumpsResponse.ProtoReflect.Descriptor instead.
+func (*GetPumpsResponse) Descriptor() ([]byte, []int) {
+	return file_gobonsai_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *GetPumpsResponse) GetPumps() []*PumpData {
+	if x != nil {
+		return x.Pumps
+	}
+	return nil
+}
+
+type Empty struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *Empty) Reset() {
+	*x = Empty{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_gobonsai_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Empty) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Empty) ProtoMessage() {}
+
+func (x *Empty) ProtoReflect() protoreflect.Message {
+	mi := &file_gobonsai_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Empty.ProtoReflect.Descriptor instead.
+func (*Empty) Descriptor() ([]byte, []int) {
+	return file_gobonsai_proto_rawDescGZIP(), []int{4}
+}
+
 var File_gobonsai_proto protoreflect.FileDescriptor
 
 var file_gobonsai_proto_rawDesc = []byte{
@@ -161,12 +301,24 @@ var file_gobonsai_proto_rawDesc = []byte{
 	0x6d, 0x70, 0x49, 0x64, 0x12, 0x16, 0x0a, 0x06, 0x61, 0x63, 0x74, 0x69, 0x76, 0x65, 0x18, 0x02,
 	0x20, 0x01, 0x28, 0x08, 0x52, 0x06, 0x61, 0x63, 0x74, 0x69, 0x76, 0x65, 0x12, 0x18, 0x0a, 0x07,
 	0x73, 0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0x18, 0x03, 0x20, 0x01, 0x28, 0x08, 0x52, 0x07, 0x73,
-	0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0x32, 0x50, 0x0a, 0x0f, 0x47, 0x6f, 0x42, 0x6f, 0x6e, 0x73,
-	0x61, 0x69, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x3d, 0x0a, 0x07, 0x53, 0x65, 0x74,
-	0x50, 0x75, 0x6d, 0x70, 0x12, 0x18, 0x2e, 0x67, 0x6f, 0x62, 0x6f, 0x6e, 0x73, 0x61, 0x69, 0x2e,
-	0x53, 0x65, 0x74, 0x50, 0x75, 0x6d, 0x70, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x16,
-	0x2e, 0x67, 0x6f, 0x62, 0x6f, 0x6e, 0x73, 0x61, 0x69, 0x2e, 0x53, 0x65, 0x74, 0x50, 0x75, 0x6d,
-	0x70, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x22, 0x00, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0x22, 0x3a, 0x0a, 0x08, 0x50, 0x75, 0x6d, 0x70, 0x44, 0x61,
+	0x74, 0x61, 0x12, 0x16, 0x0a, 0x06, 0x70, 0x75, 0x6d, 0x70, 0x49, 0x64, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x05, 0x52, 0x06, 0x70, 0x75, 0x6d, 0x70, 0x49, 0x64, 0x12, 0x16, 0x0a, 0x06, 0x61, 0x63,
+	0x74, 0x69, 0x76, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x08, 0x52, 0x06, 0x61, 0x63, 0x74, 0x69,
+	0x76, 0x65, 0x22, 0x3c, 0x0a, 0x10, 0x47, 0x65, 0x74, 0x50, 0x75, 0x6d, 0x70, 0x73, 0x52, 0x65,
+	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x28, 0x0a, 0x05, 0x50, 0x75, 0x6d, 0x70, 0x73, 0x18,
+	0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x12, 0x2e, 0x67, 0x6f, 0x62, 0x6f, 0x6e, 0x73, 0x61, 0x69,
+	0x2e, 0x50, 0x75, 0x6d, 0x70, 0x44, 0x61, 0x74, 0x61, 0x52, 0x05, 0x70, 0x75, 0x6d, 0x70, 0x73,
+	0x22, 0x07, 0x0a, 0x05, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x32, 0x8b, 0x01, 0x0a, 0x0f, 0x47, 0x6f,
+	0x42, 0x6f, 0x6e, 0x73, 0x61, 0x69, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x3d, 0x0a,
+	0x07, 0x53, 0x65, 0x74, 0x50, 0x75, 0x6d, 0x70, 0x12, 0x18, 0x2e, 0x67, 0x6f, 0x62, 0x6f, 0x6e,
+	0x73, 0x61, 0x69, 0x2e, 0x53, 0x65, 0x74, 0x50, 0x75, 0x6d, 0x70, 0x52, 0x65, 0x71, 0x75, 0x65,
+	0x73, 0x74, 0x1a, 0x16, 0x2e, 0x67, 0x6f, 0x62, 0x6f, 0x6e, 0x73, 0x61, 0x69, 0x2e, 0x53, 0x65,
+	0x74, 0x50, 0x75, 0x6d, 0x70, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x22, 0x00, 0x12, 0x39, 0x0a, 0x08,
+	0x47, 0x65, 0x74, 0x50, 0x75, 0x6d, 0x70, 0x73, 0x12, 0x0f, 0x2e, 0x67, 0x6f, 0x62, 0x6f, 0x6e,
+	0x73, 0x61, 0x69, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x1a, 0x1a, 0x2e, 0x67, 0x6f, 0x62, 0x6f,
+	0x6e, 0x73, 0x61, 0x69, 0x2e, 0x47, 0x65, 0x74, 0x50, 0x75, 0x6d, 0x70, 0x73, 0x52, 0x65, 0x73,
+	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -181,19 +333,25 @@ func file_gobonsai_proto_rawDescGZIP() []byte {
 	return file_gobonsai_proto_rawDescData
 }
 
-var file_gobonsai_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_gobonsai_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_gobonsai_proto_goTypes = []interface{}{
-	(*SetPumpRequest)(nil), // 0: gobonsai.SetPumpRequest
-	(*SetPumpReply)(nil),   // 1: gobonsai.SetPumpReply
+	(*SetPumpRequest)(nil),   // 0: gobonsai.SetPumpRequest
+	(*SetPumpReply)(nil),     // 1: gobonsai.SetPumpReply
+	(*PumpData)(nil),         // 2: gobonsai.PumpData
+	(*GetPumpsResponse)(nil), // 3: gobonsai.GetPumpsResponse
+	(*Empty)(nil),            // 4: gobonsai.Empty
 }
 var file_gobonsai_proto_depIdxs = []int32{
-	0, // 0: gobonsai.GoBonsaiService.SetPump:input_type -> gobonsai.SetPumpRequest
-	1, // 1: gobonsai.GoBonsaiService.SetPump:output_type -> gobonsai.SetPumpReply
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	2, // 0: gobonsai.GetPumpsResponse.Pumps:type_name -> gobonsai.PumpData
+	0, // 1: gobonsai.GoBonsaiService.SetPump:input_type -> gobonsai.SetPumpRequest
+	4, // 2: gobonsai.GoBonsaiService.GetPumps:input_type -> gobonsai.Empty
+	1, // 3: gobonsai.GoBonsaiService.SetPump:output_type -> gobonsai.SetPumpReply
+	3, // 4: gobonsai.GoBonsaiService.GetPumps:output_type -> gobonsai.GetPumpsResponse
+	3, // [3:5] is the sub-list for method output_type
+	1, // [1:3] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_gobonsai_proto_init() }
@@ -226,6 +384,42 @@ func file_gobonsai_proto_init() {
 				return nil
 			}
 		}
+		file_gobonsai_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PumpData); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_gobonsai_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GetPumpsResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_gobonsai_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Empty); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -233,7 +427,7 @@ func file_gobonsai_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_gobonsai_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -260,6 +454,7 @@ const _ = grpc.SupportPackageIsVersion6
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type GoBonsaiServiceClient interface {
 	SetPump(ctx context.Context, in *SetPumpRequest, opts ...grpc.CallOption) (*SetPumpReply, error)
+	GetPumps(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*GetPumpsResponse, error)
 }
 
 type goBonsaiServiceClient struct {
@@ -279,9 +474,19 @@ func (c *goBonsaiServiceClient) SetPump(ctx context.Context, in *SetPumpRequest,
 	return out, nil
 }
 
+func (c *goBonsaiServiceClient) GetPumps(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*GetPumpsResponse, error) {
+	out := new(GetPumpsResponse)
+	err := c.cc.Invoke(ctx, "/gobonsai.GoBonsaiService/GetPumps", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GoBonsaiServiceServer is the server API for GoBonsaiService service.
 type GoBonsaiServiceServer interface {
 	SetPump(context.Context, *SetPumpRequest) (*SetPumpReply, error)
+	GetPumps(context.Context, *Empty) (*GetPumpsResponse, error)
 }
 
 // UnimplementedGoBonsaiServiceServer can be embedded to have forward compatible implementations.
@@ -290,6 +495,9 @@ type UnimplementedGoBonsaiServiceServer struct {
 
 func (*UnimplementedGoBonsaiServiceServer) SetPump(context.Context, *SetPumpRequest) (*SetPumpReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SetPump not implemented")
+}
+func (*UnimplementedGoBonsaiServiceServer) GetPumps(context.Context, *Empty) (*GetPumpsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPumps not implemented")
 }
 
 func RegisterGoBonsaiServiceServer(s *grpc.Server, srv GoBonsaiServiceServer) {
@@ -314,6 +522,24 @@ func _GoBonsaiService_SetPump_Handler(srv interface{}, ctx context.Context, dec 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _GoBonsaiService_GetPumps_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GoBonsaiServiceServer).GetPumps(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/gobonsai.GoBonsaiService/GetPumps",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GoBonsaiServiceServer).GetPumps(ctx, req.(*Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _GoBonsaiService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "gobonsai.GoBonsaiService",
 	HandlerType: (*GoBonsaiServiceServer)(nil),
@@ -321,6 +547,10 @@ var _GoBonsaiService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SetPump",
 			Handler:    _GoBonsaiService_SetPump_Handler,
+		},
+		{
+			MethodName: "GetPumps",
+			Handler:    _GoBonsaiService_GetPumps_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
